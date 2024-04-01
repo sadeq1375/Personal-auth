782b573ef8cba93cebee69e6135e3b50538d4522
sadeq1375 <123751187+sadeq1375@users.noreply.github.com>
2024-04-01 15:22:44 +0430

    implementing login

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ import {
   authRoutes,
   publicRoutes,
 } from "@/routes";
+
 const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   const { nextUrl } = req;
@@ -16,15 +17,19 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return null;
+    return;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    return null;
+    return;
   }
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+  return;
 });
 
 export const config = {
