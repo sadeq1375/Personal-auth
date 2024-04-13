f80984962017edfcd81b8506366345c521c2670b
sadeq1375 <123751187+sadeq1375@users.noreply.github.com>
2024-04-13 16:02:15 +0430

    implementing settings

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,15 +1,46 @@
 "use client";
+import * as z from "zod";
 import { settings } from "@/actions/settings";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { useTransition } from "react";
+import { useSession } from "next-auth/react";
+import { useState, useTransition } from "react";
 import { FcSettings } from "react-icons/fc";
+import { Form, useForm } from "react-hook-form";
+import { SettingsSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const SettingsPage = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+  const { update } = useSession();
   const [isPending, startTransition] = useTransition();
-  const onClick = () => {
+  const form = useForm<z.infer<typeof SettingsSchema>>({
+    resolver: zodResolver(SettingsSchema),
+    defaultValues: { name: "" },
+  });
+  const onSubmit = (values: z.infer<typeof SettingsSchema>) => {
     startTransition(() => {
-      settings({ name: "New Name!" });
+      settings(values)
+        .then((data) => {
+          if (data.error) {
+            setError(data.error);
+          }
+          if (data.success) {
+            update();
+            setSuccess(data.success);
+          }
+        })
+        .catch(() => {
+          setError("Something went wrong! try again later");
+        });
     });
   };
   return (
@@ -21,9 +52,25 @@ const SettingsPage = () => {
         </p>
       </CardHeader>
       <CardContent>
-        <Button onClick={onClick} disabled={isPending}>
-          Update name
-        </Button>
+        <Form {...form}>
+          <form
+            className="space-y-6"
+            onClick={() => form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={(field) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="sadegh" />
+                  </FormControl>
+                </FormItem>
+              )}
+            ></FormField>
+          </form>
+        </Form>
       </CardContent>
     </Card>
   );
--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import { db } from "@/lib/db";
 import { getUserById } from "./data/user";
 import { UserRole } from "@prisma/client";
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
+import { getAccountByUserId } from "./data/account";
 
 export const {
   handlers: { GET, POST },
@@ -56,12 +57,22 @@ export const {
       if (session.user) {
         session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean;
       }
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email as string;
+        session.user.isOAuth = token.isOAuth as boolean;
+      }
+
       return session;
     },
     async jwt({ token }) {
       if (!token.sub) return token;
       const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
+      const existingAccount = await getAccountByUserId(existingUser.id);
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
       token.role = existingUser.role;
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
 
