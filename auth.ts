072d1a6683069a8c11138ad5f492f4d1006ba312
sadeq1375 <123751187+sadeq1375@users.noreply.github.com>
2024-04-14 20:57:17 +0430

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -67,7 +67,7 @@ export const {
 
       if (session.user) {
         session.user.name = token.name;
-        session.user.email = token.email;
+        session.user.email = token.email as string;
         session.user.isOAuth = token.isOAuth as boolean;
       }
 
